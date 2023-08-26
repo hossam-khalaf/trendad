7f18a98a97c678f42e293f35176dddf78ac6d810
hossam-khalaf <hossamhassan303089@gmail.com>
2023-08-26 23:38:50 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+   theme:{
+    colors: {
+      'metal': '#0EC9AC',
+    }
+   }
+  },
+  plugins: [],
+}
